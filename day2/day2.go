c6c0003041e7314c4461efb8a14ef206a5c359f1
Brandon Bachynski <brandon.bachynski@gmail.com>
2020-12-02 21:59:48 -0700

    Day 2

---
+++ day2/day2.go
@@ -0,0 +1,96 @@
+package day2
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func ReadFile() {
+	fileName := os.Args[1]
+
+	fileBytes, err := ioutil.ReadFile(fileName)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	sliceData := strings.Split(string(fileBytes), "\n")
+
+	validatePasswords(sliceData)
+
+}
+
+func validatePasswords(data []string) {
+	var counter1 = 0
+	var counter2 = 0
+
+	for _,item := range data {
+		key, input, nums := organizeData(item)
+
+		if isValidPassword1(key, input, nums) {
+			counter1++
+		}
+
+		if isValidPassword2(key, input, nums) {
+			counter2++
+		}
+	}
+
+	fmt.Println(counter1)
+	fmt.Println(counter2)
+
+}
+
+func organizeData(data string) (string, string, []int){
+
+	items := strings.Split(data, " ")
+	var nums []int
+
+	numbers := regexp.MustCompile(`[\d]+`)
+	num := numbers.FindAllString(items[0], -1)
+
+	for _, i := range num {
+		j, err := strconv.Atoi(i)
+		if err != nil {
+			panic(err)
+		}
+		nums = append(nums, j)
+	}
+
+	key := strings.Split(items[1], ":")
+
+	return key[0], items[2], nums
+
+}
+
+func isValidPassword1(key string, input string, validRange []int) bool {
+	var counter = 0
+	var low = validRange[0]
+	var high = validRange[1]
+
+	for _,i := range input {
+		if string(i) == key {
+			counter++
+		}
+	}
+
+	if counter >= low && counter <= high {
+		return true
+	}
+
+	return false
+
+}
+
+func isValidPassword2(key string, input string, positions []int) bool {
+	var position1 = positions[0] - 1
+	var position2 = positions[1] - 1
+
+	return (string(input[position1]) == key && string(input[position2]) != key) || (string(input[position2]) == key && string(input[position1]) != key)
+
+}
--- main.go
+++ main.go
@@ -1,65 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"os"
-	"strconv"
-	"strings"
-)
+import "github.com/schoolboybru/advent-of-code-2020/day2"
 
 const target = 2020
 
 func main() {
-	numbers := readFile()
+	// Day 1 use day_1_input.txt as arg
+	//day1.ReadFile()
 
-	part1(numbers, target)
-
-	part2(numbers, target)
-
+	// Day 1 use day_2_input.txt as arg
+	//Day 2
+	day2.ReadFile()
 }
 
-func part2 (numbers []int, target int) {
-	m := make(map[int]int)
-
-	for _, num := range numbers {
-		m[num] = num
-		for value := range m {
-			if val, found := m[target - num - value]; found {fmt.Println(val * num * value)}
-		}
-	}
-}
-
-func part1 (numbers []int, target int) {
-	m := make(map[int]int)
-	for _, num := range numbers {
-		var newValue = target - num
-		if val, found := m[target - num]; found { fmt.Println(val * num)}
-		m[num] = target - newValue
-	}
-}
-
-func readFile() []int {
-	fileName := os.Args[1]
-
-	fileBytes, err := ioutil.ReadFile(fileName)
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	sliceData := strings.Split(string(fileBytes), "\n")
-
-	var numbers []int
-
-	for _,i := range sliceData {
-		j, err := strconv.Atoi(i)
-			if err != nil {
-				panic(err)
-			}
-			numbers = append(numbers, j)
-	}
-
-	return numbers
-}
